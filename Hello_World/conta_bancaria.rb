829d7166b2149992f2efe7941c359715d1739cef
Felipe Correia de Jesus <fecorjesus@gmail.com>
2024-02-02 07:01:08 -0300

    exercio pratico

---
+++ Hello_World/conta_bancaria.rb
@@ -0,0 +1,9 @@
+#Exercicio pratico
+
+# Criar uma classe responsavel para contas bancarias
+# Criar um metodo que me permita transferir valor entre contas usando
+#"conta1.transferir(conta2,100)" onde os 100 é o valor que desejo transferir.
+#Plus:"Criar um tipo de conta em que existe uma tarifa para se tranaferir dinheiro"
+
+
+
