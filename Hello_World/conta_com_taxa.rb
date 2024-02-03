a16e5d196ba11b7828b6eb96846e83412e120eb4
Felipe Correia de Jesus <fecorjesus@gmail.com>
2024-02-03 09:08:05 -0300

    Filanizando introdução a objetos

--- Hello_World/conta_com_taxa.rb
+++ Hello_World/conta_com_taxa.rb
@@ -1,9 +1,19 @@
 #conta_com_taxa.rb
 
 class ContaComTaxa < ContaBancaria
+    #poderia ficar assim
+    #def self.taxa
+    #   2
+    #end
+    
+    
     def transferir(outra_conta, valor)
-        debitar(2)#valor debitado da conta
         super
+        #debitar(ContaBancaria.taxa)
+        #trocaria pela de baixo.
+        taxa_de_saque = 2
+        debitar(taxa_de_saque)
+
     end
 end
 
