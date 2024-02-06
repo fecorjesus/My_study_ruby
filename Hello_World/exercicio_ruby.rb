0f32495212a8c83c10b3f3e7aa992166e340e85b
Felipe Correia de Jesus <fecorjesus@gmail.com>
2024-02-06 07:01:40 -0300

    Converter idade em dias Ppotugues/ingles

--- Hello_World/exercicio_ruby.rb
+++ Hello_World/exercicio_ruby.rb
@@ -14,5 +14,34 @@ puts "Número original: #{numero_original}"
 puts "Novo número: #{novo_numero}"
 =end
 
+#Converter idade em dias
+=begin
+def idade_em_dias(idade_em_anos)
+    dias_por_ano = 365
+    idade_em_dias = idade_em_anos * dias_por_ano
+    return idade_em_dias
+  end
+  
+  # Exemplo de uso
+  idade_em_anos = 46
+  idade_em_dias_resultado = idade_em_dias(idade_em_anos)
+  puts "Idade em anos: #{idade_em_anos}"
+  puts "Idade em dias: #{idade_em_dias_resultado}"
+=end
+
+#Teste com a sintese em ingles
+=begin
+def age_in_days(age_in_years)
+    days_per_year = 365
+    age_in_days = age_in_years * days_per_year
+    return age_in_days
+  end
+  
+  # Example of usage
+  age_in_years = 25
+  age_in_days_result = age_in_days(age_in_years)
+  puts "Age in years: #{age_in_years}"
+  puts "Age in days: #{age_in_days_result}"
+=end
 
 
