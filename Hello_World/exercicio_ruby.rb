3784d026f4ef0599ca28e66d54d9cb60f2082a78
Felipe Correia de Jesus <fecorjesus@gmail.com>
2024-02-06 06:24:31 -0300

    foi trabalhoso mais, fiz rodar esse servidor rails

---
+++ Hello_World/exercicio_ruby.rb
@@ -0,0 +1,17 @@
+#exercicio_ruby.rb
+#Retorne o próximo número do número inteiro passado
+
+=begin
+def aumenta_em_um(numero)
+    resultado = numero + 1
+    return resultado
+end
+  
+# Exemplo de uso
+numero_original = 6
+novo_numero = aumenta_em_um(numero_original)
+puts "Número original: #{numero_original}"
+puts "Novo número: #{novo_numero}"
+=end
+
+
